28fcef133a5c054d8e3e53dec000455f0b9e8dcc
Brandon Artner <bartner1@hotmail.com>
2017-09-24 18:24:55 -0700

    Really trying to fix everything

--- Stepper_Controller_BLE.ino
+++ Stepper_Controller_BLE.ino
@@ -1,5 +1,5 @@
 /***********************************************************************
-* 	FILENAME :        Stepper_Controller_BT.ino
+* 	FILENAME :        Stepper_Controller_BLE.ino
 *
 * 	DESCRIPTION :
 *       Arduino code for running a stepper motor at specific rpms via bluetooth. The motor's 
@@ -29,39 +29,25 @@
 #include <Stepper.h>
 #include <CurieBLE.h>
 
+#define DEBUG true 	// Display BTSerial messages on Serial Monitor
+#define MAXIMUM_RPM 1200; 	// Maximum rpm of the motor, our motor shut off if set to a higher speed
+#define STEPS_PER_REVOLUTION 200;  	// change this to fit the number of steps per revolution for your motor
+
 BLEPeripheral blePeripheral; // create BLE peripheral instance
 
 BLEService stepperService("19B10000-E8F2-537E-4F6C-D104768A1214"); // create BLE service
 
-// create switch characteristic and allow remote device to read and write
+// create characteristic for stepper rpm and allow remote device to read and write
 BLEIntCharacteristic stepperRPM("19B10001-E8F2-537E-4F6C-D104768A1215", BLERead | BLEWrite);
 
-// create switch characteristic and allow remote device to read and write
+// create characteristic for the ramping speed and allow remote device to read and write
 BLEIntCharacteristic rampingSpeed("19B10002-E8F2-537E-4F6C-D104768A1215", BLERead | BLEWrite);
 
-//define variables
-#define DEBUG true //display BTSerial messages on Serial Monitor
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(STEPS_PER_REVOLUTION, 8, 9, 10, 11);	
 
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution for your motor
-// initialize the stepper library on pins 8 through 11: 
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
-
-const int MAXIMUM_RPM = 1200;
 int rpm = 0;
 
-// Auxiliary functions
-//********************
-void blePeripheralConnectHandler(BLECentral& central) {
-  // central connected event handler
-  Serial.print("Connected event, central: ");
-  Serial.println(central.address());
-}
-
-void blePeripheralDisconnectHandler(BLECentral& central) {
-  // central disconnected event handler
-  Serial.print("Disconnected event, central: ");
-  Serial.println(central.address());
-}
 
 //**************
 // Arduino Setup
@@ -123,11 +109,28 @@ void loop() {
   }
   // Don't run if rpm is 0, will cause program to hang
   if(stepperRPM.value() != 0)
-    myStepper.step(stepsPerRevolution);
+    myStepper.step(STEPS_PER_REVOLUTION);
 }
 
 
 //********************
+// Auxiliary functions
+//********************
+void blePeripheralConnectHandler(BLECentral& central) {
+  // central connected event handler
+  if(DEBUG) {
+	  Serial.print("Connected event, central: ");
+	  Serial.println(central.address());
+  }
+}
+
+void blePeripheralDisconnectHandler(BLECentral& central) {
+  // central disconnected event handler
+  if(DEBUG) {
+	  Serial.print("Disconnected event, central: ");
+	  Serial.println(central.address());
+  }
+}
 
 void stepperRPMWritten(BLECentral& central, BLECharacteristic& characteristic) {
   // central wrote new value to characteristic, update LED
