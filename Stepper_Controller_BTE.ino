8675734faaa4448f8166f994ef4a9d32a6bb33b1
Brandon Artner <bartner1@hotmail.com>
2017-09-24 17:01:31 -0700

    + A bunch of comments + Functionality to alter the acceleration of the motor

--- Stepper_Controller_BTE.ino
+++ Stepper_Controller_BTE.ino
@@ -1,15 +1,43 @@
-// Written By: Brandon Artner
+/***********************************************************************
+* 	FILENAME :        Stepper_Controller_BT.ino
+*
+* 	DESCRIPTION :
+*       Arduino code for running a stepper motor at specific rpms via bluetooth. The motor's 
+*		rpm is ramped to the desired speed using a user set acceleration.
+*
+* 	PUBLIC FUNCTIONS :
+*       void blePeripheralConnectHandler( BLECentral& )
+*		void blePeripheralDisconnectHandler( BLECentral& )
+*		void setup( )
+*		void stepperRPMWritten( BLECentral& , BLECharacteristic& )
+*		
+*
+* 	NOTES :
+*       - Uses the Curie Bluetooth Low-Energy library for bluetooth connection.
+*		- We used nRF Connect from Nordic Semiconductor on Android appliction in order to send and recieve commands.
+*
+*
+* 	AUTHOR :    Brandon Artner        START DATE :    June 2017
+*
+* 	CHANGES :
+*
+*		DATE    	WHO     DETAIL
+*  		24Sep2017 	BA      Added all of these super handy comments
+*
+**/
 
-//include libraries
 #include <Stepper.h>
 #include <CurieBLE.h>
 
-BLEPeripheral blePeripheral; // create peripheral instance
+BLEPeripheral blePeripheral; // create BLE peripheral instance
 
-BLEService stepperService("19B10000-E8F2-537E-4F6C-D104768A1214"); // create service
+BLEService stepperService("19B10000-E8F2-537E-4F6C-D104768A1214"); // create BLE service
 
 // create switch characteristic and allow remote device to read and write
 BLEIntCharacteristic stepperRPM("19B10001-E8F2-537E-4F6C-D104768A1215", BLERead | BLEWrite);
+
+// create switch characteristic and allow remote device to read and write
+BLEIntCharacteristic rampingSpeed("19B10002-E8F2-537E-4F6C-D104768A1215", BLERead | BLEWrite);
 
 //define variables
 #define DEBUG true //display BTSerial messages on Serial Monitor
@@ -18,7 +46,7 @@ const int stepsPerRevolution = 200;  // change this to fit the number of steps p
 // initialize the stepper library on pins 8 through 11: 
 Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 
-const int maxrpm = 1200;
+const int MAXIMUM_RPM = 1200;
 int rpm = 0;
 
 // Auxiliary functions
@@ -55,6 +83,7 @@ void setup()
   // add service and characteristic
   blePeripheral.addAttribute(stepperService);
   blePeripheral.addAttribute(stepperRPM);
+  blePeripheral.addAttribute(rampingSpeed);
 
   // assign event handlers for connected, disconnected to peripheral
   blePeripheral.setEventHandler(BLEConnected, blePeripheralConnectHandler);
@@ -68,6 +97,7 @@ void setup()
   
   // set an initial value for the characteristic
   stepperRPM.setValue(0);
+  rampingSpeed.setValue(100);
   
   // advertise the service
   blePeripheral.begin();
@@ -83,10 +113,13 @@ void loop() {
 
   // If the current rpm is less than desired rpm increase by 100
   if(rpm<stepperRPM.value()){
-    rpm+=100;
+    rpm+=rampingSpeed.value();
     myStepper.setSpeed(rpm);
     Serial.print("clockwise,rpm: ");
     Serial.println(rpm);
+  }
+  if(rpm>stepperRPM.value()){
+	rpm = stepperRPM.value();
   }
   // Don't run if rpm is 0, will cause program to hang
   if(stepperRPM.value() != 0)
@@ -100,13 +133,13 @@ void stepperRPMWritten(BLECentral& central, BLECharacteristic& characteristic) {
   // central wrote new value to characteristic, update LED
   Serial.print("Characteristic event, written: ");
   
-  if(stepperRPM.value() > maxrpm){
-    char* buf = (char*) malloc(sizeof(char)*100);
-    sprintf(buf,"RPM entered is greater than maximum allowed. (Max RPM = %d)",maxrpm);
-    Serial.println(buf);
+  if(stepperRPM.value() > MAXIMUM_RPM){
+	char* buffTemplate
+    char* buff = (char*) malloc(sizeof(char)*100);
+    sprintf(buff,"RPM entered is greater than maximum allowed. (Max RPM = %d)",MAXIMUM_RPM);
+    Serial.println(buff);
     stepperRPM.setValue(0);
   }
   else
     rpm=0; 
 }
-
